8a79672ec4dd673948c694b007bebf7342a1fcb3
Quillinan <milton.quillinan1798@gmail.com>
2023-06-07 11:12:49 -0300

    signup

--- src/components/Sign-up/Sign-upPage.jsx
+++ src/components/Sign-up/Sign-upPage.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/");
+  };
+
   return (
     <PageContainer>
-      <p>SignUp</p>
+      <input placeholder="Nome" />
+      <input placeholder="CPF" />
+      <input placeholder="E-mail" />
+      <input placeholder="Senha" />
+      <button>CADASTRAR</button>
+      <p onClick={handleLoginClick}>Já possui uma conta? Entre</p>
     </PageContainer>
   );
 }
 
 const PageContainer = styled.div`
-  font-size: 24px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 150px;
+  p {
+    font-family: "Roboto";
+    font-size: 14px;
+    font-weight: 400;
+    margin-top: 24px;
+    text-decoration-line: underline;
+    color: #ffffff;
+  }
 `;
